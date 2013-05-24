95f76e9136878f61760804d34c42637966b09e2d
Alex Selby <alex.selby@pobox.com>
2013-05-24 22:58:59 +0100

    Added nibble-wise representation and did byte-wise (K_4,4-wise) optimisation

--- qubo.c
+++ qubo.c
@@ -19,14 +19,16 @@
 // i is the index within the "semi-K4,4"
 
 #define L 3
-#define N 8
+#define N 4
 // Require 2^L >= N, but N doesn't have to be a power of 2
 
 #define HORIZ 3
 #define VERT (1<<(L+3))
 #define M (1<<(2*L+3))
-#define NV (N*N*8)
+#define NV (8*N*N)
 #define NE (8*N*(3*N-1))
+#define NBV (2*N*N)
+#define NBE (N*(3*N-2))
 #define enc(x,y,o,i) ((i)+((o)<<2)+((x)<<3)+((y)<<(L+3)))
 #define decx(p) (((p)>>3)&((1<<L)-1))
 #define decy(p) (((p)>>(L+3))&((1<<L)-1))
@@ -37,8 +39,8 @@
 // \|/\|/|\|
 //  |  | | Bits 0,1 = i
 //  |  | Bit 2 = o
-//  |  Bits 3...(L+2) = x
-//  Bit (L+3)...(2L+2) = y
+//  |  Bits 3..(L+2) = x
+//  Bit (L+3)..(2L+2) = y
 
 // Convention is that smaller numbers always correspond to horizontal
 
@@ -46,16 +48,21 @@ int adj[M][6];// Neighbours as encoded vertices (-1 for non-existent)
               // 0-3 corresponds to intra-K_4,4 neighbours
               // 4 = Left or Down, 5 = Right or Up
 int Q[M][6];// Weights
-int X[M];// State (0 or 1)
 int vlist[NV]; // encoded vertex list
 int elist[NE][4]; // elist[e][0] = encoded start vertex of edge e
-                  // elist[e][1] = edge number (0...5) from vertex elist[e][0] to elist[e][2]
+                  // elist[e][1] = edge number (0..5) from vertex elist[e][0] to elist[e][2]
                   // elist[e][2] = encoded end vertex of edge e
-                  // elist[e][3] = edge number (0...5) from vertex elist[e][2] to elist[e][0]
+                  // elist[e][3] = edge number (0..5) from vertex elist[e][2] to elist[e][0]
+int XB[N][N][2];// State (0..15).  Index is (x,y,o) = big vert
+int QB[N][N][2][16][16][3]; // Weights for big verts (derived from Q[])
+                            // QC[x][y][o][s0][s1][d] = total weight from big vert (x,y,o) in state s0
+                            //                          to the big vert in direction d in state s1
+                            // d=0 is intra K_4,4, d=1 is Left/Down, d=2 is Right/Up
+#define X(p) ((XB[decx(p)][decy(p)][deco(p)]>>deci(p))&1)
 
 void initrand(){srandom(time(0));}
-
 int randbit(void){return (random()>>16)&1;}
+int randnib(void){return (random()>>16)&15;}
 
 void initgraph(void){
   int i,j,o,p,q,x,y,nv,ne;
@@ -84,14 +91,30 @@ void initgraph(void){
 
 void initweights(void){// Initialise a symmetric weight matrix with random +/-1s
   int i,j;
-  for(i=0;i<NV;i++)for(j=0;j<6;j++)Q[i][j]=0; // To ensure weight=0 for non-existent edges
+  for(i=0;i<NV;i++)for(j=0;j<6;j++)Q[i][j]=0; // Ensure weight=0 for non-existent edges
   for(i=0;i<NE;i++)
     Q[elist[i][0]][elist[i][1]]=Q[elist[i][2]][elist[i][3]]=randbit()*2-1;
 }
 
-void initspins(void){// Initialise state with random 0,1s
-  int i;
-  for(i=0;i<NV;i++)X[vlist[i]]=randbit();
+void getbigweights(void){// Get derived weights on "big graph" QB[] from Q[]
+  int i,j,o,p,t,x,y,s0,s1;
+  for(x=0;x<N;x++)for(y=0;y<N;y++)for(o=0;o<2;o++)for(s0=0;s0<16;s0++)for(s1=0;s1<16;s1++){
+    p=enc(x,y,o,0);
+    t=0;
+    for(i=0;i<4;i++)for(j=0;j<4;j++)t+=Q[p+i][j]*((s0>>i)&1)*((s1>>j)&1);
+    QB[x][y][o][s0][s1][0]=t;
+    t=0;
+    for(i=0;i<4;i++)t+=Q[p+i][4]*((s0>>i)&1)*((s1>>i)&1);
+    QB[x][y][o][s0][s1][1]=t;
+    t=0;
+    for(i=0;i<4;i++)t+=Q[p+i][5]*((s0>>i)&1)*((s1>>i)&1);
+    QB[x][y][o][s0][s1][2]=t;
+  }
+}
+
+void init_state(void){// Initialise state randomly
+  int x,y,o;
+  for(x=0;x<N;x++)for(y=0;y<N;y++)for(o=0;o<2;o++)XB[x][y][o]=randnib();
 }
 
 void writeweights(char *f){
@@ -111,11 +134,12 @@ void writeweights(char *f){
 }
 
 void readweights(char *f){
-  int i,w,x0,y0,o0,i0,e0,x1,y1,o1,i1,e1,nx,ny;
+  int i,j,w,x0,y0,o0,i0,e0,x1,y1,o1,i1,e1,nx,ny;
   FILE *fp;
   fp=fopen(f,"r");assert(fp);
   assert(fscanf(fp,"%d %d",&nx,&ny)==2);
   assert(nx==N&&ny==N);
+  for(i=0;i<NV;i++)for(j=0;j<6;j++)Q[i][j]=0; // Ensure weight=0 for non-existent edges
   for(i=0;i<NE;i++){
     assert(fscanf(fp,"%d %d %d %d %d %d %d %d %d %d %d",
                   &x0,&y0,&o0,&i0,&e0,
@@ -127,16 +151,23 @@ void readweights(char *f){
 }
 
 int val(void){
-  int i,v;
+  int v,x,y;
   v=0;
-  for(i=0;i<NE;i++)v+=Q[elist[i][0]][elist[i][1]]*X[elist[i][0]]*X[elist[i][2]];
+  for(x=0;x<N;x++)for(y=0;y<N;y++){
+    v+=QB[x][y][0][XB[x][y][0]][XB[x][y][1]][0];
+    if(x<N-1)v+=QB[x][y][0][XB[x][y][0]][XB[x+1][y][0]][2];
+    if(y<N-1)v+=QB[x][y][1][XB[x][y][1]][XB[x][y+1][1]][2];
+  }
   return v;
 }
 
 int dval(int p){
-  int j,v;
+  int j,v,v0;
   v=0;
-  for(j=0;j<6;j++)if(adj[p][j]>=0)v+=Q[p][j]*X[adj[p][j]];
+  for(j=0;j<6;j++){
+    v0=adj[p][j];
+    if(v0>=0)v+=Q[p][j]*X(v0);
+  }
   return v;
 }
 
@@ -146,21 +177,28 @@ int main(int ac,char**av){
   initgraph();
   if(ac<2){initweights();printf("Initialising random weight matrix\n");}else
     {readweights(av[1]);printf("Reading weight matrix from file \"%s\"\n",av[1]);}
+  getbigweights();
   writeweights("tempproblem");
-  int i,p,r,bv,cv,dv;
+  int r,v,x,y,s0,s1,bv,cv,vmin;
   long long int nn;
   bv=1000000000;nn=0;
   while(1){
-    initspins();
+    init_state();
     cv=val();
-    r=0;
-    while(r<NV){
-      for(i=0;i<NV;i++){
-        p=vlist[i];
-        dv=dval(p)*(1-2*X[p]);
-        if(dv<0){cv+=dv;X[p]=1-X[p];r=0;} else {r++;if(r==NV)break;}
+    do{
+      for(x=0;x<N;x++)for(y=0;y<N;y++){
+        vmin=1000000000;
+        for(s0=0;s0<16;s0++)for(s1=0;s1<16;s1++){
+          v=QB[x][y][0][s0][s1][0];
+          if(x>0)v+=QB[x][y][0][s0][XB[x-1][y][0]][1];
+          if(x<N-1)v+=QB[x][y][0][s0][XB[x+1][y][0]][2];
+          if(y>0)v+=QB[x][y][1][s1][XB[x][y-1][1]][1];
+          if(y<N-1)v+=QB[x][y][1][s1][XB[x][y+1][1]][2];
+          if(v<vmin){vmin=v;XB[x][y][0]=s0;XB[x][y][1]=s1;}
+        }
       }
-    }
+      v=val();r=(v<cv);if(r)cv=v;
+    }while(r);
     nn++;
     if(cv<bv||((nn&(nn-1))==0)){
       if(cv<bv)bv=cv;
